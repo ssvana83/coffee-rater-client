d58c48eb81bbcb8278dacf8646b6cd2e5b2ef617
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-11-08 17:05:31 -0500

    fixed

--- src/components/MyReviews.js
+++ src/components/MyReviews.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import { useContext } from 'react'
 import { UserContext } from '../context/user'
+import ReviewCard from './ReviewCard'
+
 
 const MyReviews = () => {
+  
   const { user, setUser } = useContext(UserContext)
-  const renderMyReviews = user.reviews.map(r => <li key={r.id}>{r.content}</li>)
+  const renderMyReviews = user.reviews.map(r => <ReviewCard key={r.id} review={r} />)
+  
   return (
     <div>{renderMyReviews}</div>
   )
--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,17 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect }from 'react'
 import ReviewForm from './ReviewForm'
+import { useContext } from 'react'
+import { UserContext } from '../context/user'
 
 
 const ReviewCard = ({ review }) => {
+  const { user, setUser } = useContext(UserContext)
   
-  const { content, user } =review;
   const username = review.username
+  console.log(user)
   
+
+  const [showButton, setShowButton] = useState(false)
+
+  useEffect(() => {
+    if (user.id == review.user_id) {
+      setShowButton(true)
+    }
+  }, []) 
 
   return (
     <div className="card">
       <h2 className="p" >{review.content}</h2>
       <p className="p" >Review written by: {username}</p>
+      {showButton? (
+        <button>Update my Review</button>
+      ) : (null)}
     </div>
   )
 }
